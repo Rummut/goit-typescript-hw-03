4e63af32752c225fa9e3ce81eec2394549b6511f
Rummut <kzantip@gmail.com>
2023-10-18 00:18:54 +0300

    added all solved tasks

--- src/2.ts
+++ src/2.ts
@@ -15,16 +15,11 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +27,14 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(amount: number) {
+    super("manager", "sales", amount + 10000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
+const manager = new Manager(500);
+
+console.log(manager.getEmployeeDetails());
 
 export {};
